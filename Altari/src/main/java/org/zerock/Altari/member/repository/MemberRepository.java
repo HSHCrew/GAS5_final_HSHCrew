411058b0c9a7406128987e496486baf41a58a9cf
DESKTOP-PG84ZB2\USER <hyungj1590@naver.com>
2024-10-23 15:05:03 +0900

    필요없는 파일 삭제

--- Altari/src/main/java/org/zerock/Altari/member/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package org.zerock.Altari.member.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.zerock.Altari.member.entity.MemberEntity;
-
-public interface MemberRepository extends JpaRepository<MemberEntity, String> {
-}
--- Altari/src/main/java/org/zerock/Altari/user/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package org.zerock.Altari.user.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.zerock.Altari.user.entity.UserEntity;
-
-public interface UserRepository extends JpaRepository<UserEntity, String> {
-}
